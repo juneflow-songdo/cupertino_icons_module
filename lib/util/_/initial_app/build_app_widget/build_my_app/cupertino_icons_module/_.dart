5686110c0a96e5ab8424f6eddfdbd07c3d9ba9a8
june lee <melodysdreamj@gmail.com>
2024-03-21 17:10:45 +0900

    Initial commit

---
+++ lib/util/_/initial_app/build_app_widget/build_my_app/cupertino_icons_module/_.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../../../../../../main.dart';
+
+// @CoverMyApp()
+// Widget Function() coverCupertinoIconsModule(Widget Function() materialAppBuilder) {
+//   return () => materialAppBuilder();
+// }
---
+++ lib/util/_/initial_app/ready_functions/before_material_app/cupertino_icons_module/_.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../main.dart';
+
+// @ReadyBeforeMaterialApp()
+// Future<void> readyForCupertinoIconsModule(BuildContext context) async {}
